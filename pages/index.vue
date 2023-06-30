23fc9ce013531b4691289a7fff757714d86d2630
arisas <arisa@arisaknits.com>
2023-06-30 12:53:05 -0700

    adding row counter web app to git

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="text-center my-8">
+        <img
+      alt="ArisaKnits Row Counter App Icon of Skeins and a Cake of Yarn"
+      title="ArisaKnits Row Counter App Icon of Skeins and a Cake of Yarn"
+      class="relative mx-auto rounded-full"
+      decoding="auto"
+      src="~assets/img/icon_234.png"
+      style="object-fit: cover; max-width: 400px"
+      loading="lazy"
+    />
+    <h1 class="text-3xl font-extrabold my-8">ArisaKnits Row Counter Web App</h1>
+        <p class="text-gray-500 text-xl my-5">A free web app!</p>
+    </div>
+
+    <div class="relative h-32 w-32 text-3xl">
+        <button class="absolute inset-y-0 left-0 w-16">
+            ➕
+        </button>
+        <button class="absolute inset-y-0 right-0 w-16">
+            ➖
+        </button>
+    </div>
+
+
+
+</template>
